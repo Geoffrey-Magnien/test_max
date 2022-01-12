766852ee07701d7fb0dfd4de897ea73c02dcc84b
Geoffrey-Magnien <78850881+Geoffrey-Magnien@users.noreply.github.com>
2022-01-12 15:55:16 +0100

    Delete compile_max.sh

--- compile_max.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc -Wall -c max.c -lcunit
-
-
-gcc -Wall -L/D:\INSTALLER\CUnit-2.1-3/CUnit/Sources/.libs -I/D:\INSTALLER\CUnit-2.1-3/CUnit/Headers -o max_test max_test.c max.o -lcunit
-
